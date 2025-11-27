524d4608ed0917659fdbc0fd9cff287780fc2872
Paolo Pecis <paolo@loka.com>
2025-11-27 17:08:32 +0000

    Fixing tools call

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -2,7 +2,7 @@ import { SparklesIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
   AutoModel,
   AutoTokenizer,
-  Message,
+  type Message,
   PreTrainedModel,
   PreTrainedTokenizer,
 } from "@huggingface/transformers";
@@ -15,6 +15,8 @@ import mdToHtml from "../../utils/converter/mdToHtml.ts";
 import { MODELS, SYSTEM_PROMPT } from "../../utils/llm/constants.ts";
 import {
   WebMCPTool,
+  executeToolCall,
+  extractToolCalls,
   webMCPToolToChatTemplateTool,
 } from "../../utils/llm/webMcp.ts";
 import ChatForm from "./ChatForm.tsx";
@@ -22,39 +24,54 @@ import ChatForm from "./ChatForm.tsx";
 const Chat: FC = () => {
   const [chatOpen, setChatOpen] = useState<boolean>(false);
 
+  const [conversation, setConversation] = useState<Array<Message>>([]);
+
   const [thinking, setThinking] = useState<boolean>(false);
+
   const [response, setResponse] = useState<string>("");
+
   const [callbackElements, setCallbackElements] = useState<Array<ReactElement>>(
     []
   );
 
-  const [conversation, setConversation] = useState<Array<Message>>([]);
-
   const tools: Array<WebMCPTool> = [
     {
       name: "get_page_context",
+
       description:
         "Get the current page context. Often the user navigates through the page so use this tool each time the user requests information about the current page or item",
+
       inputSchema: {
         type: "object",
+
         properties: {},
+
         required: [],
       },
+
       execute: async () => {
         return `Current Page: ${pageContext.title}
-                  ${pageContext.content}`;
+
+  ${pageContext.content}`;
       },
     },
   ];
 
   const pipe = useRef<{
     tokenizer: PreTrainedTokenizer;
+
     model: PreTrainedModel;
   }>(null);
 
   const { pageContext } = usePageContext();
 
-  const onAskLLM = async (question: string): Promise<string> => {
+  console.log(pageContext);
+
+  const runLLM = async (
+    prompt: string,
+
+    role: "tool" | "user" = "user"
+  ): Promise<string> => {
     const model = MODELS.granite3B;
 
     const messages = conversation;
@@ -62,59 +79,105 @@ const Chat: FC = () => {
     if (messages.length == 0) {
       messages.push({
         role: "system",
+
         content: SYSTEM_PROMPT,
       });
     }
 
     messages.push({
-      role: "user",
-      content: question,
+      role,
+
+      content: prompt,
     });
 
     if (!pipe.current) {
       const tokenizer = await AutoTokenizer.from_pretrained(model.modelId);
-      const pretrainedModel: PreTrainedModel = await AutoModel.from_pretrained(
-        model.modelId,
-        {
-          device: model.device,
-          dtype: model.dtype,
-        }
-      );
+
+      const preTrainedModel = await AutoModel.from_pretrained(model.modelId, {
+        device: model.device,
+
+        dtype: model.dtype,
+      });
 
       pipe.current = {
         tokenizer,
-        model: pretrainedModel,
+
+        model: preTrainedModel,
       };
     }
 
-    const input: any = pipe.current.tokenizer.apply_chat_template(messages, {
+    console.log(messages);
+
+    //const resp = await pipe.current(messages, { max_new_tokens: 1000 });
+
+    const input = pipe.current.tokenizer.apply_chat_template(messages, {
       tools: tools.map(webMCPToolToChatTemplateTool),
+
       add_generation_prompt: true,
+
       return_dict: true,
     });
 
-    const { sequences }: any = await pipe.current.model.generate({
+    const { sequences } = await pipe.current.model.generate({
       ...input,
+
       max_new_tokens: 1000,
+
       return_dict_in_generate: true,
     });
 
-    console.log(messages);
+    const lengthOfInput: number = input.input_ids.dims[1];
 
-    const lengthOfInput: number = input.input_ids_dims[1];
-    const response = pipe.current.tokenizer.batch_decode(
-      sequences.slice(null, [lengthOfInput, Number.MAX_SAFE_INTEGER], {
-        skip_special_tokens: true,
-      })
-    )[0];
+    const response = pipe.current.tokenizer
 
-    console.log(response);
+      .batch_decode(
+        sequences.slice(null, [lengthOfInput, Number.MAX_SAFE_INTEGER], {
+          skip_special_tokens: true,
+        })
+      )[0]
+      .replace("<|end_of_text|>", "");
 
-    messages.push({ role: "assistant", content: response });
+    messages.push({
+      role: "assistant",
+
+      content: response,
+    });
 
     setConversation(messages);
 
     return response;
+  };
+
+  const onAskLLM = async (question: string): Promise<string> => {
+    let prompt = question;
+
+    let role: "tool" | "user" = "user";
+
+    const responses = [];
+
+    while (prompt) {
+      const response = await runLLM(prompt, role);
+
+      const { toolCalls, message } = extractToolCalls(response);
+
+      if (toolCalls.length === 0) {
+        prompt = "";
+
+        responses.push(message);
+      } else {
+        const toolResponses = await Promise.all(
+          toolCalls.map(
+            async (toolCall) => await executeToolCall(toolCall, tools)
+          )
+        );
+
+        prompt = toolResponses.map((response) => response.result).join("\n\n");
+      }
+
+      role = "tool";
+    }
+
+    return responses.join("\n\n");
   };
 
   return (
@@ -122,6 +185,7 @@ const Chat: FC = () => {
       <div
         className={cn(
           "fixed right-4 bottom-24 flex w-md origin-bottom-right flex-col gap-4 rounded-lg border border-purple-400 bg-purple-50 p-6 shadow-xl transition duration-300",
+
           {
             "translate-x-0 translate-y-16 scale-15 opacity-0": !chatOpen,
           }
@@ -130,21 +194,28 @@ const Chat: FC = () => {
         <h3 className="flex items-center gap-2">
           <SparklesIcon aria-hidden="true" className="size-4" /> Ask the Agent
         </h3>
+
         <ChatForm
           chatOpen={chatOpen}
           onSubmit={async (prompt) => {
             if (!prompt) {
               setResponse("");
+
               setCallbackElements([]);
+
               return;
             }
 
             setThinking(true);
+
             const resp = await onAskLLM(prompt);
+
             setResponse(resp);
+
             setThinking(false);
           }}
         />
+
         {(response.length !== 0 || thinking) && (
           <div className="mt-4">
             {thinking ? (
@@ -154,6 +225,7 @@ const Chat: FC = () => {
             ) : (
               <div className="flex flex-col gap-4">
                 {callbackElements.map((element) => element)}
+
                 <div
                   className="font-light text-gray-700 [&>li]:ml-5 [&>ol]:my-2 [&>ol]:ml-4 [&>ol]:list-decimal [&>ul]:my-2 [&>ul]:ml-5 [&>ul]:list-disc"
                   dangerouslySetInnerHTML={{ __html: mdToHtml(response) }}
@@ -163,6 +235,7 @@ const Chat: FC = () => {
           </div>
         )}
       </div>
+
       <button
         onClick={() => setChatOpen((open) => !open)}
         className="fixed right-4 bottom-4 grid cursor-pointer rounded-full bg-purple-900 p-3 text-white outline-2 outline-offset-4 outline-purple-300 transition hover:outline-4 hover:outline-purple-900 focus:outline-4 focus:outline-purple-900"
@@ -173,6 +246,7 @@ const Chat: FC = () => {
             "rotate-90 opacity-0": !chatOpen,
           })}
         />
+
         <SparklesIcon
           aria-hidden="true"
           className={cn("col-start-1 row-start-1 size-8 transition", {
